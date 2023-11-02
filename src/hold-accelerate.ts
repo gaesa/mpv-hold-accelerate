faf93f482f8b170c6df66c44b0a1f6036e46d056
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-02 11:09:10 +0800

    refactor: properly use `namespace` to make code modular

--- src/hold-accelerate.ts
+++ src/hold-accelerate.ts
@@ -8,49 +8,56 @@ namespace Config {
   export const slowSpeed: number = 0.5;
 }
 
-interface CallbackOnKey {
-  event?: string;
-  is_mouse?: boolean;
-  key_name?: string;
-}
-
 namespace SpeedPlayback {
-  export let prevSpeed = mp.get_property_native("speed");
-  export let isPlaying = false;
-}
-
-function makePlayFn(speedValue: number) {
-  return function (table: CallbackOnKey): void {
-    if (table.event === "down" || table.event === "repeat") {
-      SpeedPlayback.isPlaying = true;
-      mp.set_property("speed", speedValue);
-      mp.osd_message(`>> x${speedValue.toFixed(2)}`, Config.osdDuration);
-    } else if (table.event === "up") {
-      mp.set_property("speed", SpeedPlayback.prevSpeed);
-      mp.osd_message(
-        `${SpeedPlayback.prevSpeed.toFixed(2)}x`,
-        Config.osdDuration,
-      );
-      SpeedPlayback.isPlaying = false;
-    } else {
-      return;
-    }
-  };
-}
-
-mp.observe_property("speed", "number", (_: string, value: number) => {
-  if (SpeedPlayback.isPlaying) {
-    return;
-  } else {
-    SpeedPlayback.prevSpeed = value;
+  interface Input {
+    event?: string;
+    is_mouse?: boolean;
+    key_name?: string;
   }
-});
 
-mp.add_key_binding("=", "hold-accelerate@fast", makePlayFn(Config.fastSpeed), {
-  complex: true,
-  repeatable: false,
-});
-mp.add_key_binding("-", "hold-accelerate@slow", makePlayFn(Config.slowSpeed), {
-  complex: true,
-  repeatable: false,
-});
+  export function make(speedValue: number) {
+    return function (table: Input): void {
+      if (table.event === "down" || table.event === "repeat") {
+        isPlaying = true;
+        mp.set_property("speed", speedValue);
+        mp.osd_message(`>> x${speedValue.toFixed(2)}`, Config.osdDuration);
+      } else if (table.event === "up") {
+        mp.set_property("speed", prevSpeed);
+        mp.osd_message(`${prevSpeed.toFixed(2)}x`, Config.osdDuration);
+        isPlaying = false;
+      } else {
+        return;
+      }
+    };
+  }
+
+  let prevSpeed = mp.get_property_native("speed");
+  let isPlaying = false;
+
+  mp.observe_property("speed", "number", (_: string, value: number) => {
+    if (isPlaying) {
+      return;
+    } else {
+      prevSpeed = value;
+    }
+  });
+}
+
+mp.add_key_binding(
+  "=",
+  "hold-accelerate@fast",
+  SpeedPlayback.make(Config.fastSpeed),
+  {
+    complex: true,
+    repeatable: false,
+  },
+);
+mp.add_key_binding(
+  "-",
+  "hold-accelerate@slow",
+  SpeedPlayback.make(Config.slowSpeed),
+  {
+    complex: true,
+    repeatable: false,
+  },
+);
