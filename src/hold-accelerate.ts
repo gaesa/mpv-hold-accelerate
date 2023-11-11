26b7717a72baa3444afee1a7dd7560c4558e146c
gaesa <71256557+gaesa@users.noreply.github.com>
2023-11-11 22:47:48 +0800

    chore: add default values for `mp.get_property_native`

--- src/hold-accelerate.ts
+++ src/hold-accelerate.ts
@@ -2,7 +2,7 @@ namespace Config {
     const decayDelay = 0.05;
     export const osdDuration = Math.max(
         decayDelay,
-        mp.get_property_native("osd-duration") / 1000,
+        mp.get_property_native("osd-duration", 1000) / 1000,
     );
     export const fastSpeed = 2.5; // a higher value like `3` is more likely to cause `Audio/Video desynchronisation`
     export const slowSpeed = 0.5;
@@ -34,7 +34,7 @@ namespace SpeedPlayback {
         };
     }
 
-    let prevSpeed: number = mp.get_property_native("speed");
+    let prevSpeed: number = mp.get_property_native("speed", 1);
     let isPlaying = false;
 
     mp.observe_property("speed", "number", (_: string, value: number) => {
